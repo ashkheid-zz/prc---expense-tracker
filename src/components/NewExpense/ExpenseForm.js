b6f558bde75f0d1ddccccde52e21f3d416e9dbea
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-23 15:00:45 +0330

    Trying to clear userinput after submitting

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,10 +45,16 @@ const ExpenseForm = () => {
     const expenseData = {
       title: userInput.enteredTitle,
       amount: userInput.enteredAmount,
-      date: new Date(userInput.enteredDate)
-    }
-    
+      date: new Date(userInput.enteredDate),
+    };
+
     console.log(expenseData);
+    
+    setUserInput({
+      enteredTitle: '',
+      enteredAmount: '',
+      enteredDate: '',
+    });
   };
 
   return (
@@ -56,7 +62,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -64,6 +74,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={userInput.enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -73,6 +84,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={userInput.enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
