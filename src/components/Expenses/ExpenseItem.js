e432531f1e5fc120e0fe4597f91c768cf4b2a6d7
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-23 11:45:41 +0330

    Changing title
    with the help of useState hook and handleing a click event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("I'm New Here!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>change title</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
